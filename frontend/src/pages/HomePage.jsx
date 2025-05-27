06e73feea700e01a7bf179c5a847706c49954003
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-05-27 13:21:15 +0800

    fix: protect homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Income } from '../components/Income'
 import { Expense } from '../components/Expense'
 import { TransactionType } from '../components/TransactionType'
 import { TotalIncome } from '../components/TotalIncome'
 import { TotalExpenses } from '../components/TotalExpenses'
+import { useAuth } from '../components/context/AuthContext'
+import { useNavigate } from 'react-router-dom'
 
 export const HomePage = () => {
+    const { userIsAuthenticated } = useAuth()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (!userIsAuthenticated()) {
+            navigate('/login')
+        }
+    }, [userIsAuthenticated, navigate])
+
+    if (!userIsAuthenticated()) {
+        return null
+    }
 
     return (
         <div className='gap-y-4 grid'>
